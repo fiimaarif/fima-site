a839dfc5971613be542ace2d46d9eef350007441
fiimaarif <fii.ma'arif@baf.id>
2023-09-20 11:51:11 +0700

    [UPDATE] Meta Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <meta name="description" content="I am fii maarif a frontend developer, I have experience in creating and developing responsive and intuitive UI." />
       <meta property="og:title" content="fii maarif personal website"/>
       <meta property="og:description" content="I am fii maarif a frontend developer, I have experience in creating and developing responsive and intuitive UI."/>
-      <meta property="og:image" content="favicon.png"/>
+      <meta property="og:image" content="https://fiimaarif-kgf4t5hq6-fiimaarif.vercel.app/favicon.png"/>
     </Head>
     <Navbar/>
     <Hero/>
